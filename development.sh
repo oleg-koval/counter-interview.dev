e744b43d8fd0bc79347ae17512c7bc2473f87768
Oleg Koval <kvl.olg@gmail.com>
2019-04-08 22:07:42 +0200

    Fix for crashing styles

--- development.sh
+++ development.sh
@@ -1,2 +1,2 @@
-bundle exec jekyll serve -I --watch --config "_config.yml,_config_dev.yml"
-# bundle exec jekyll serve
+# bundle exec jekyll serve -I --watch --config "_config.yml,_config_dev.yml"
+bundle exec jekyll serve
